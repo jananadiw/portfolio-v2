e126e39ab267ca5bcb61abfe653c54fd9d8ff7c9
jananadiw <jananadiw@gmail.com>
2024-09-09 00:54:36 +0200

    chore: update footer

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -5,8 +5,8 @@ export default function FooterComponent() {
   return (
     <div>
       <p className={`${outfit.className} text-slate-300 text-lg `}>
-        Design inspired by Brittany Chiang. Coded in Visual Studio Code & Built
-        using NextJs and Tailwind CSS.
+        Made with 🩷 Coded in Visual Studio Code & Built using NextJs and
+        Tailwind CSS.
       </p>
     </div>
   );
