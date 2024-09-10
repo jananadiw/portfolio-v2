47333dca4c7295540f39762e68e7703807c38ee9
jananadiw <jananadiw@gmail.com>
2024-09-10 15:19:33 +0200

    fix: Add missing type to a parameter

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -30,7 +30,7 @@ export default function HeaderComponent() {
     };
   }, []);
 
-  const handleLinkClick = (link) => {
+  const handleLinkClick = (link: string) => {
     setActiveLink(link);
   };
 
