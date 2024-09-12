f9225a0261f1d42b982b5e87f663ce07d86b66ab
jananadiw <jananadiw@gmail.com>
2024-09-12 17:16:19 +0200

    fix: Console warnings due to wrapped div with Link

--- app/components/work.tsx
+++ app/components/work.tsx
@@ -28,77 +28,76 @@ export default function WorkComponent() {
 
   useEffect(() => {
     getData().then((data) => setExperience(data));
-  }, []);
+  }, [getData]);
+
+  const handleItemClick = (url: string) => {
+    window.open(url, "_blank", "noopener,noreferrer");
+  };
 
   return (
     <section
       id="work"
-      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-32 lg:scroll-mt-24 "
+      className="mb-16 scroll-mt-16 md:mb-24 lg:mb-32 lg:scroll-mt-24"
     >
       {loading ? (
         <Loading componentName={"work"} />
       ) : (
         experience.map((item: any, index: number) => (
-          <Link
+          <div
             key={index}
-            target="_blank"
-            rel="noreferrer noopener"
-            href={`${item.url}`}
+            className="group p-4 rounded-lg transition hover:bg-test5 hover:drop-shadow-2xl flex mb-10 cursor-pointer"
+            onClick={() => handleItemClick(item.url)}
           >
-            <div className="group p-4 rounded-lg transition hover:bg-test5 hover:drop-shadow-2xl flex mb-10">
-              <motion.div
-                className="flex w-full p-4"
-                whileHover={{ scale: 1.05, transition: { duration: 0.1 } }}
+            <motion.div
+              className="flex w-full p-4"
+              whileHover={{ scale: 1.05, transition: { duration: 0.1 } }}
+            >
+              <div
+                className={`w-1/4 text-slate-300 text-md ${outfit.className}`}
               >
+                {item.time}
+              </div>
+              <div
+                className={`text-test1 text-lg ${outfit.className} w-3/4 ml-4`}
+              >
+                <h3 className="text-slate-300 group-hover:text-red-300">
+                  {item.title} - {item.workplace}
+                </h3>
+                <p className="mt-4 text-test1">{item.detail}</p>
                 <div
-                  className={`w-1/4 text-slate-300 text-md ${outfit.className}`}
+                  className={`mt-4 flex flex-row gap-4 ${outfit.className} text-base md:text-sm text-slate-100`}
                 >
-                  {item.time}
+                  {item.projects.map((project: any, i: number) => (
+                    <div
+                      key={i}
+                      className="flex items-center group-hover:text-red-300"
+                    >
+                      <PaperClipIcon className="h-4 w-4 mr-1" />
+                      <Link
+                        href={`${project.url}`}
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        {project.name}
+                      </Link>
+                    </div>
+                  ))}
                 </div>
                 <div
-                  className={`text-test1 text-lg ${outfit.className} w-3/4 ml-4`}
+                  className={`mt-6 flex flex-wrap gap-4 ${outfit.className} text-sm`}
                 >
-                  <h3 className="text-slate-300 group-hover:text-red-300">
-                    {item.title} - {item.workplace}
-                  </h3>
-                  <p className="mt-4 text-test1">{item.detail}</p>
-                  <div
-                    className={`mt-4 flex flex-row gap-4 ${outfit.className} text-base md:text-sm text-slate-100`}
-                  >
-                    {item.projects.map((project: any, i: number) => (
-                      <div
-                        key={i}
-                        className="flex items-center group-hover:text-red-300"
-                      >
-                        <>
-                          <PaperClipIcon className="h-4 w-4 mr-1" />
-                          <Link
-                            href={`${project.url}`}
-                            target="_blank"
-                            rel="noopener noreferrer"
-                          >
-                            {project.name}
-                          </Link>
-                        </>
-                      </div>
-                    ))}
-                  </div>
-                  <div
-                    className={`mt-6 flex flex-wrap gap-4 ${outfit.className} text-sm`}
-                  >
-                    {item.stack.map((tech: string, i: number) => (
-                      <div
-                        key={i}
-                        className="relative flex select-none items-center whitespace-nowrap rounded-lg bg-cyan-900 py-0.5 px-2 text-base text-green-100"
-                      >
-                        {tech}
-                      </div>
-                    ))}
-                  </div>
+                  {item.stack.map((tech: string, i: number) => (
+                    <div
+                      key={i}
+                      className="relative flex select-none items-center whitespace-nowrap rounded-lg bg-cyan-900 py-0.5 px-2 text-base text-green-100"
+                    >
+                      {tech}
+                    </div>
+                  ))}
                 </div>
-              </motion.div>
-            </div>
-          </Link>
+              </div>
+            </motion.div>
+          </div>
         ))
       )}
       <ViewMore text={"See Detailed Resume"} type={"work"} />
