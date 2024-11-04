ac213b1298efd857508bf55c808809b4dd3481fa
jananadiw <jananadiw@gmail.com>
2024-11-04 09:47:31 +0100

    refactor: Move cursor component

---
+++ app/components/Cursor.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect } from "react";
+import { motion, useMotionValue, useSpring } from "framer-motion";
+
+export default function Cursor() {
+  const cursorX = useMotionValue(-100);
+  const cursorY = useMotionValue(-100);
+
+  const springConfig = { damping: 40, stiffness: 650 };
+  const cursorXSpring = useSpring(cursorX, springConfig);
+  const cursorYSpring = useSpring(cursorY, springConfig);
+
+  useEffect(() => {
+    const moveCursor = (e: MouseEvent) => {
+      cursorX.set(e.pageX);
+      cursorY.set(e.pageY);
+    };
+
+    window.addEventListener("mousemove", moveCursor);
+    return () => window.removeEventListener("mousemove", moveCursor);
+  }, [cursorX, cursorY]);
+
+  return (
+    <motion.div
+      className="cursor"
+      style={{
+        translateX: cursorXSpring,
+        translateY: cursorYSpring,
+      }}
+    />
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect } from "react";
-import { motion, useMotionValue, useSpring } from "framer-motion";
+import { motion, useMotionValue, useSpring, useTransform } from "framer-motion";
 
 import About from "@/components/About";
 import DataProvider from "@/components/DataProvider";
@@ -9,28 +9,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function Home() {
-  const cursorX = useMotionValue(-100);
-  const cursorY = useMotionValue(-100);
-
-  const springConfig = { damping: 25, stiffness: 700 };
-  const cursorXSpring = useSpring(cursorX, springConfig);
-  const cursorYSpring = useSpring(cursorY, springConfig);
-
-  useEffect(() => {
-    const moveCursor = (
-      e: React.MouseEvent<Element, MouseEvent> | MouseEvent
-    ) => {
-      cursorX.set(e.clientX - 16);
-      cursorY.set(e.clientY + window.scrollY - 16); // Adjust for scroll position
-    };
-
-    window.addEventListener("mousemove", moveCursor);
-
-    return () => {
-      window.removeEventListener("mousemove", moveCursor);
-    };
-  }, []);
-
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 font-sans md:px-12 md:py-20 lg:px-24 lg:py-0 text-white">
       <div className="lg:flex lg:justify-between lg:gap-4">
@@ -42,13 +20,6 @@ export default function Home() {
           <Footer />
         </div>
       </div>
-      <motion.div
-        className="cursor"
-        style={{
-          translateX: cursorXSpring,
-          translateY: cursorYSpring,
-        }}
-      />
     </div>
   );
 }
